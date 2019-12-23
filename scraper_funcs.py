90e0ad5bbf70616e64665afa31df1e873515ae72
Emil Elgaard <shivan@shivan.dk>
2019-12-23 10:56:19 +0100

    removed deleted code

--- scraper_funcs.py
+++
@@ -1,9 +0,0 @@
-def get_last_page(soup):
-    '''takes a soup as input and and returns false if nothing is found, otherwise returns link to last page'''
-
-    last = soup.select('ul.pages > .last_page > .page_num')
-
-    if len(last)>0:
-        return last[0].attrs['href']
-    else:
-        return False
